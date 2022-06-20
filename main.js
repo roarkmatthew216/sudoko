4c406109d4a970ecc5c9b5c52685d499aaabc4ce
DESKTOP-0H57ULF\Matt <matthewroark216@gmail.com>
2022-06-20 11:41:17 -0500

    can know show one offs

--- main.js
+++ main.js
@@ -1,91 +1,84 @@
 const sudoL = require("./sudukoLogic.js");
 
-
 const printBoard = (board) => {
-    topCell = " --- ";
-    bottomCell = " --- ";
-    leftCell = "| ";
-    rightCell = " |"
-    newLine = "\n";
+  topCell = " --- ";
+  bottomCell = " --- ";
+  leftCell = "| ";
+  rightCell = " |";
+  newLine = "\n";
 
+  outLine = "";
+
+  for (i = 0; i <= 18; i++) {
     outLine = "";
-
-    for (i = 0; i <= 18; i++) {
-        outLine = "";
-        for (j = 0; j < 9; j++) {
-            if (i % 2 == 0) {
-                outLine = outLine + topCell;
-            } else {
-                let index = `${(i+1)/2}-${j+1}`;
-                outLine = outLine + leftCell;
-                if (board.get(index) == " ") {
-                    outLine = outLine + " ";
-                } else {
-                    outLine = outLine + board.get(index);
-                }
-
-
-                outLine = outLine + rightCell;
-            }
-
-
+    for (j = 0; j < 9; j++) {
+      if (i % 2 == 0) {
+        outLine = outLine + topCell;
+      } else {
+        let index = `${(i + 1) / 2}-${j + 1}`;
+        outLine = outLine + leftCell;
+        if (board.get(index) == " ") {
+          outLine = outLine + " ";
+        } else {
+          outLine = outLine + board.get(index);
         }
-        console.log(outLine)
-    }
 
-}
+        outLine = outLine + rightCell;
+      }
+    }
+    console.log(outLine);
+  }
+};
 
 const createBoard = (board, startingInput) => {
-    for (i = 0; i < 9; i++) {
-        for (j = 0; j < 9; j++) {
-            let index = `${i+1}-${j+1}`
-            if (typeof(startingInput[index]) != "undefined") {
-                board.set(index, startingInput[index])
-            } else {
-                board.set(index, " ")
-            }
-
-
-        }
+  for (i = 0; i < 9; i++) {
+    for (j = 0; j < 9; j++) {
+      let index = `${i + 1}-${j + 1}`;
+      if (typeof startingInput[index] != "undefined") {
+        board.set(index, startingInput[index]);
+      } else {
+        board.set(index, " ");
+      }
     }
-}
-
-
-
-
-
-
+  }
+};
 
 const board = new Map();
 let startingInput = {
-    '1-8': 7,
-    '2-3': 4,
-    '2-4': 8,
-    '3-1': 1,
-    '3-3': 9,
-    '3-4': 4,
-    '3-5': 6,
-    '3-8': 3,
-    '4-1': 4,
-    '4-5': 1,
-    '5-2': 5,
-    '5-5': 2,
-    '6-2': 1,
-    '6-4': 3,
-    '6-6': 6,
-    '6-7': 4,
-    '7-8': 9,
-    '7-9': 8,
-    '8-1': 5,
-    '8-7': 6,
-    '8-9': 7,
-    '9-1': 2,
-    '9-4': 1,
+  "1-8": 7,
+  "2-3": 4,
+  "2-4": 8,
+  "3-1": 1,
+  "3-3": 9,
+  "3-4": 4,
+  "3-5": 6,
+  "3-8": 3,
+  "4-1": 4,
+  "4-5": 1,
+  "5-2": 5,
+  "5-5": 2,
+  "6-2": 1,
+  "6-4": 3,
+  "6-6": 6,
+  "6-7": 4,
+  "7-8": 9,
+  "7-9": 8,
+  "8-1": 5,
+  "8-7": 6,
+  "8-9": 7,
+  "9-1": 2,
+  "9-4": 1,
 };
 
-createBoard(board, startingInput)
+createBoard(board, startingInput);
 printBoard(board);
 
+
+
+
+
 for (let i = 0; i < 9; i++) {
-    console.log(sudoL.checkAllPossibleCells(board, i + 1));
+  let onePossible = sudoL.checkAllPossibleCells(board, i+1);
+
+  sudoL.checkOneOffs(onePossible,i+1);
 }
--- sudukoLogic.js
+++ sudukoLogic.js
@@ -1,5 +1,5 @@
 //does number exist in row
-
+const csColors = require("./consoleColors")
 
 const checkArray = (number, array) => {
 
@@ -13,11 +13,52 @@ const checkArray = (number, array) => {
     return false;
 }
 
+const getTriCellNumber = (row,col)=>{
+ 
+    if (1 <= row && row <= 3) {
+
+        if (1 <= col && col <= 3) {
+            return  1;
+        }
+        if (4 <= col && col <= 6) {
+            return  2;
+        }
+        if (7 <= col && col <= 9) {
+            return 3;
+        }
+    }
+    if (4 <= row && row <= 6) {
+
+        if (1 <= col && col <= 3) {
+            return  4;
+        }
+        if (4 <= col && col <= 6) {
+            return  5;
+        }
+        if (7 <= col && col <= 9) {
+            return 6;
+        }
+    }
+    if (7 <= row && row <= 9) {
+
+        if (1 <= col && col <= 3) {
+            return  7;
+        }
+        if (4 <= col && col <= 6) {
+            return  8;
+        }
+        if (7 <= col && col <= 9) {
+            return 9;
+        }
+    }
+    
+
+}
 
 
 
 
-const checkCellifGood = (board, index, number) => {
+const checkCellIfGood = (board, index, number) => {
 
     // if num exist in row return false;
     if (board.get(index) != " ") {
@@ -102,15 +143,15 @@ const checkCellifGood = (board, index, number) => {
 }
 
 const checkAllPossibleCells = (board, number) => {
-    console.log(`value:${number} is not prevented to go in`)
+    console.log(`checking one offs for :${number} `)
     let indexCount = 0;
     let allPossibleCells = new Array();
     for (i = 0; i < 9; i++) {
         for (j = 0; j < 9; j++) {
             //board.get(`${i+1}-${j+1}`);
-            // checkCellifGood(board, `${i+1}-${j+1}`, number);
+            // checkCellIfGood(board, `${i+1}-${j+1}`, number);
 
-            if (checkCellifGood(board, `${i+1}-${j+1}`, number)) {
+            if (checkCellIfGood(board, `${i+1}-${j+1}`, number)) {
                 allPossibleCells[indexCount] = `${i+1}-${j+1}`;
                 indexCount++;
             }
@@ -122,15 +163,69 @@ const checkAllPossibleCells = (board, number) => {
     return allPossibleCells;
 
 }
+const checkOneOffs=(array,number)=>{
+    // how many instance where only one number can fit in a row, col or tirCell
+    let rowMap = new Map();
+    let colMap = new Map();
+    let triMap = new Map();
 
-const possibleRows = () => {};
-const possibleCellsInRow = () => {};
+    const setValue = (map,tag)=>{
+        let outputMap  = map;
+        if(typeof(outputMap.get(`${tag}`))=="undefined"){
+            outputMap.set(`${tag}`,1)
+          
+        }else{
+            outputMap.set(`${tag}`,Number(outputMap.get(`${tag}`))+1)
+        }
+        return outputMap;
+    }
 
-const possibleCols = () => {};
-const possibleCellsInCol = () => {};
+    for(let i =0 ;i<array.length;i++){
+        //gets index
+        index = array[i];
+        row = index.substring(0,1);
+        col = index.substring(2,3);
 
-const possibleTriCells = () => {};
-const possibleCellsInTriCells = () => {};
+        rowMap = setValue(rowMap,row);
+        colMap = setValue(colMap,col);
+        if(typeof(triMap.get(getTriCellNumber(row,col)))=="undefined"){
+            triMap.set(getTriCellNumber(row,col),1);
+        }else{
+            triMap.set(getTriCellNumber(row,col),Number(triMap.get(getTriCellNumber(row,col)))+1);
+        }
+        
+    }
+  
+ 
+    rowMap.forEach((value, key, map) => {
+        if(value ==1){
+            console.log(csColors.FgYellow,`row ${key} has ${map.get(key)} locations where ${number} can go`)
+            console.log(csColors.FgWhite)
+        }
+        // else{
+        //     console.log(`row ${key} has ${map.get(key)} locations where ${number} can go`)
+        // }
+    } )
+    colMap.forEach((value, key, map) => {
+        if(value ==1){
+            console.log(csColors.FgYellow,`col ${key} has ${map.get(key)} locations where ${number} can go`)
+            console.log(csColors.FgWhite)
+        }
+        // else{
+        //     console.log(`col ${key} has ${map.get(key)} locations where ${number} can go`)
+        // }
+    } )
+    triMap.forEach((value, key, map) => {
+        if(value ==1){
+            console.log(csColors.FgYellow,`Tri ${key} has ${map.get(key)} locations where ${number} can go`)
+            console.log(csColors.FgWhite)
+        }
+        // else{
+        //     console.log(`Tri ${key} has ${map.get(key)} locations where ${number} can go`)
+        // }
+    } )
+   
+}
 
 
-module.exports = { checkCellifGood, checkAllPossibleCells };
+module.exports = { checkCellIfGood, checkOneOffs,checkAllPossibleCells };
